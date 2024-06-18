42dd755f5e8c04fa095ab383825e8d8a1bcedd87
Giologic <gioantonvelez@gmail.com>
2024-06-18 08:18:02 +0800

    Created Pokemon Service API with Find All Pokemon and Get Pokemon by Pokemon ID

--- pokemon-service/src/pocketbase.js
+++ pokemon-service/src/pocketbase.js
@@ -1,5 +1,18 @@
-import PocketBase from 'pocketbase/cjs';
+import PocketBase from 'pocketbase';
+import dotenv from 'dotenv';
 
-const pb = new PocketBase('http://127.0.0.1:8090'); // Replace with your Pocketbase URL
+dotenv.config();
+
+const authenticate = async () => {
+    try {
+        await pb.admins.authWithPassword(process.env.POCKETBASE_EMAIL, process.env.POCKETBASE_PASSWORD);
+        console.log('Authenticated with PocketBase');
+    } catch (error) {
+        console.error('Failed to authenticate with PocketBase:', error);
+    }
+};
+
+const pb = new PocketBase(process.env.POCKETBASE_URL);
+authenticate();
 
 export default pb;
